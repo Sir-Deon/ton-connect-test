3e960b4d6b13883e078894436341234d775f065b
Sir-Deon <sird655@gmail.com>
2024-07-30 12:46:41 +0100

    made updates

--- bot/index.js
+++ bot/index.js
@@ -2,13 +2,13 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "6713396623:AAEpofgDy_GUUnxgDgojVal_5dNSAXsvUNk";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://isfoody.netlify.app";
+const web_link = "https://ton-connect-test.netlify.app";
 const bot_url= "https://t.me/bBetterbet_bot."
 
 bot.start((ctx) =>
-  ctx.reply("Welcome :))))) \n to bBetter", {
+  ctx.reply("Welcome :))))) \n to Ton connect", {
     reply_markup: {
-      inline_keyboard: [[{ text: "Visit bBetter", url: web_link  }]],
+      inline_keyboard: [[{ text: "Open App", url: web_link  }]],
 
     },
   })
